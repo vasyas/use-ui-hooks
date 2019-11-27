3115b253c6a25ce7214af436f54498095246e95d
vasyas <vasyl@stashuk.com>
2019-11-28 01:29:18 +0200

    split into ui-hooks, utils

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {useAction} from "./useAction"
 export {ResultContext, useResult} from "./result"
 export {useTopic} from "./useTopic"
-export {useForm} from "./useForm"
+export {useForm, Field} from "./useForm"
 export {usePrevious} from "./utils"
 export {Constraint, ValidateMessages} from "./validate"
