79e815e5fb11694156a3e8ddca61b32d34ab8d72
vasyas <vasyl@stashuk.com>
2020-02-21 13:50:53 +0200

    more validator

--- src/validate.ts
+++ src/validate.ts
@@ -1,6 +1,8 @@
 export interface Constraint {
   required: boolean
-  integer?: boolean
+  integer: boolean
+  number: boolean
+  nonNegative: boolean
 }
 
 export function message(constraint: Constraint, value: any, validateMessages: ValidateMessages) {
@@ -14,16 +16,24 @@ export function message(constraint: Constraint, value: any, validateMessages: Va
 
   if (value == "") return // by default field is not required
 
-  if (constraint.integer) if (!integer(value)) return validateMessages.integer
+  if (constraint.integer && !integer(value)) return validateMessages.integer
+  if (constraint.number && !number(value)) return validateMessages.number
+  if (constraint.nonNegative && +value < 0) return validateMessages.nonNegative
 }
 
 export function integer(s) {
   return /^[0-9]*$/.test(s)
 }
 
+export function number(s) {
+  return !isNaN(s)
+}
+
 export const enValidateMessages = {
   required: "Field required",
-  integer: "Numbers only",
+  integer: "Integers only",
+  number: "Numbers only",
+  nonNegative: "Numbers only",
 }
 
 export type ValidateMessages = typeof enValidateMessages
