9b62e0674cd0b2a8a948441ce35b4c9dc53f8518
vasyas <vasyl@stashuk.com>
2020-03-01 19:43:20 +0200

    useTopic api improvements

--- src/useTopic.tsx
+++ src/useTopic.tsx
@@ -2,18 +2,22 @@ import {Topic} from "push-rpc"
 import * as React from "react"
 import {useCallback, useEffect, useState} from "react"
 
-export function useTopic<D, P>(topic: Topic<D, P>, params: P = null): {data: D} {
-  const [data, setData] = useState<D>()
+export function useTopic<D, P>(topic: Topic<D, P>, params: P, def: Partial<D>): {data: Partial<D>} {
+  const [data, setData] = useState<Partial<D>>(def)
 
   const receiveData = useCallback(data => {
     setData(data)
   }, [])
 
   useEffect(() => {
-    topic.subscribe(receiveData, params)
+    if (params) {
+      topic.subscribe(receiveData, params)
+    }
 
     return () => {
-      topic.unsubscribe(params, receiveData)
+      if (params) {
+        topic.unsubscribe(params, receiveData)
+      }
     }
   }, [JSON.stringify(params)])
 
