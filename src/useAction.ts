d020e19c956f6c13bfd3b8b5490a093e76d36bef
vasyas <vasyl@stashuk.com>
2020-10-14 20:19:00 +0300

    useAction: per action tracking

--- src/useAction.ts
+++ src/useAction.ts
@@ -2,29 +2,39 @@ import {useState} from "react"
 
 export function useAction<P>(): Action<P> {
   const [error, setError] = useState<string>()
-  const [progress, setProgress] = useState(false)
+  const [progress, setProgress] = useState<number>(null)
+
+  let actionIndex = 0
 
   function action(impl: ActionImpl<P>) {
-    return async (e?, params?: P) => {
+    actionIndex++
+
+    const thisActionIndex = actionIndex
+
+    const trackedAction: TrackedAction<P> = (async (e?, params?: P) => {
       e && e.preventDefault && e.preventDefault()
 
       try {
-        setProgress(true)
+        setProgress(thisActionIndex)
         setError(null)
 
         await impl(params)
       } catch (e) {
         setError(e.message)
       } finally {
-        setProgress(false)
+        setProgress(null)
       }
-    }
+    }) as any
+
+    trackedAction.progress = progress == thisActionIndex
+
+    return trackedAction
   }
 
   return {
     error,
-    progress,
-    action,
+    progress: !!progress,
+    action: action as any,
   }
 }
 
@@ -33,5 +43,10 @@ type ActionImpl<P> = (p?: P) => Promise<void>
 export interface Action<P> {
   error: string
   progress: boolean
-  action(impl: ActionImpl<P>): (e?, params?: P) => void
+  action(impl: ActionImpl<P>): TrackedAction<P>
+}
+
+export interface TrackedAction<P> {
+  (e?, params?: P): void
+  progress: boolean
 }
